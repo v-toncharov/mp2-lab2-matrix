01e5278094fa39425d17c9cb52c1ff29dbce8646
Владислав Тончаров <Kotauskas@yandex.ru>
2025-12-01 17:00:39 +0300

    feat: adjust sample

--- samples/sample_matrix.cpp
+++ samples/sample_matrix.cpp
@@ -1,30 +1,17 @@
-﻿// ННГУ, ИИТММ, Курс "Алгоритмы и структуры данных"
-//
-// Copyright (c) Сысоев А.В.
-//
-// Тестирование матриц
-
 #include <iostream>
-#include "tmatrix.h"
-//---------------------------------------------------------------------------
+#include "owned_mat.hxx"
+using std::cout, std::endl;
 
-void main()
-{
-  TDynamicMatrix<int> a(5), b(5), c(5);
-  int i, j;
-
-  setlocale(LC_ALL, "Russian");
-  cout << "Тестирование класс работы с матрицами"
-    << endl;
-  for (i = 0; i < 5; i++)
-    for (j = i; j < 5; j++ )
-    {
-      a[i][j] =  i * 10 + j;
-      b[i][j] = (i * 10 + j) * 100;
+int main(int, char**) {
+    owned_col_mat<int> a(5, 5), b(5, 5), c(5, 5);
+    for (size_t i = 0; i < 5; i++) for (size_t j = i; j < 5; j++) {
+        a[i][j] =  (i * 10) + j;
+        b[i][j] = ((i * 10) + j) * 100;
     }
-  c = a + b;
-  cout << "Matrix a = " << endl << a << endl;
-  cout << "Matrix b = " << endl << b << endl;
-  cout << "Matrix c = a + b" << endl << c << endl;
+
+    c = a + b;
+    cout << "a:" << endl << a << endl;
+    cout << "b:" << endl << b << endl;
+    cout << "c = a + b:" << endl << c << endl;
 }
 //---------------------------------------------------------------------------
